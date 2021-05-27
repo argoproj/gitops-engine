3a25a51de49c8d6861981c3a570fbfb60eda60d9
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2021-05-26 20:20:22 -0700

    refactor: cache cmdutil.Factory to improve syncronization performance
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -84,10 +84,11 @@ func (e *gitOpsEngine) Sync(ctx context.Context,
 		return nil, err
 	}
 	opts = append(opts, sync.WithSkipHooks(!diffRes.Modified))
-	syncCtx, err := sync.NewSyncContext(revision, result, e.config, e.config, e.kubectl, namespace, opts...)
+	syncCtx, cleanup, err := sync.NewSyncContext(revision, result, e.config, e.config, e.kubectl, namespace, opts...)
 	if err != nil {
 		return nil, err
 	}
+	defer cleanup()
 
 	resUpdated := make(chan bool)
 	unsubscribe := e.cache.OnResourceUpdated(func(newRes *cache.Resource, oldRes *cache.Resource, namespaceResources map[kube.ResourceKey]*cache.Resource) {
