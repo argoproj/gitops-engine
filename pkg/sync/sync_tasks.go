3b4a500a81e3a5dc82c84813c6b3dfb558c9c19d
pashavictorovich <pavel@codefresh.io>
2021-10-20 22:30:56 +0300

    fix linter
    Signed-off-by: pashavictorovich <pavel@codefresh.io>

--- pkg/sync/sync_tasks.go
+++ pkg/sync/sync_tasks.go
@@ -197,7 +197,7 @@ func (s syncTasks) Split(predicate func(task *syncTask) bool) (trueTasks, falseT
 }
 
 func (s syncTasks) Map(predicate func(task *syncTask) string) []string {
-	messagesMap := make(map[string]interface{}, 0)
+	messagesMap := make(map[string]interface{})
 	for _, task := range s {
 		messagesMap[predicate(task)] = nil
 	}
