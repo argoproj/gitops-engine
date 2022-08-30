b7f10ef20a1a1fae13174a90aeb4b60e9904a6a0
alswl <alswlx@gmail.com>
2022-08-30 17:42:11 +0800

    feat: add GetClusterInfoInstant, support get instant snapshot counts without lock
    Signed-off-by: alswl <alswlx@gmail.com>

--- pkg/cache/cluster.go
+++ pkg/cache/cluster.go
@@ -115,6 +115,9 @@ type ClusterCache interface {
 	GetManagedLiveObjs(targetObjs []*unstructured.Unstructured, isManaged func(r *Resource) bool) (map[kube.ResourceKey]*unstructured.Unstructured, error)
 	// GetClusterInfo returns cluster cache statistics
 	GetClusterInfo() ClusterInfo
+	// GetClusterInfoInstant returns cluster cache statistics instant and without locking
+	// so the returned data may be inconsistent, and you should not modify the returned value
+	GetClusterInfoInstant() ClusterInfo
 	// OnResourceUpdated register event handler that is executed every time when resource get's updated in the cache
 	OnResourceUpdated(handler OnResourceUpdatedHandler) Unsubscribe
 	// OnEvent register event handler that is executed every time when new K8S event received
@@ -1060,6 +1063,12 @@ func (c *clusterCache) GetClusterInfo() ClusterInfo {
 	c.syncStatus.lock.Lock()
 	defer c.syncStatus.lock.Unlock()
 
+	return c.GetClusterInfoInstant()
+}
+
+// GetClusterInfoInstant returns cluster cache statistics instant and without locking
+// so the returned data may be inconsistent, and you should not modify the returned value
+func (c *clusterCache) GetClusterInfoInstant() ClusterInfo {
 	return ClusterInfo{
 		APIsCount:         len(c.apisMeta),
 		K8SVersion:        c.serverVersion,
