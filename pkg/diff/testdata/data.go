6a5b0815fb606321cb1889b96e00640874375aff
Leonardo Luz Almeida <leonardo_almeida@intuit.com>
2022-09-29 16:25:20 -0400

    better wording
    Signed-off-by: Leonardo Luz Almeida <leonardo_almeida@intuit.com>

--- pkg/diff/testdata/data.go
+++ pkg/diff/testdata/data.go
@@ -25,8 +25,8 @@ var (
 	DeploymentConfigYAML string
 
 	// OpenAPIV2Doc is a binary representation of the openapi
-	// available in a given k8s instance. To update this file
-	// the following commands can be executed:
+	// document available in a given k8s instance. To update
+	// this file the following commands can be executed:
 	//    kubectl proxy --port=7777 &
 	//    curl -s -H Accept:application/com.github.proto-openapi.spec.v2@v1.0+protobuf http://localhost:7777/openapi/v2 > openapiv2.bin
 	//
