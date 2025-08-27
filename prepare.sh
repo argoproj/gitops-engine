2cc11a808850f45dcbd41c7dd11d01def2cbc539
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-08-27 15:53:33 +0300

    fix script
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir dir1
-mv *[^dir1prepare.sh]* dir1/
+# Move all files and directories except dir1 and prepare.sh
+for item in *; do
+  if [[ "$item" != "dir1" && "$item" != "prepare.sh" ]]; then
+    mv "$item" dir1/
+  fi
+done
 git rm -r pkg specs agent internal go.* Dockerfile LICENSE OWNERS README.md docs hack codecov.yml Makefile sonar-project.properties .github/ .golangci.yaml .gitignore
 rm dir1/sonar-project.properties dir1/codecov.yml
 mv dir1/ gitops-engine
