a557220e98b3631344947bda6eb9765966668f14
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-09-04 11:36:54 +0300

    address pr comments
    Signed-off-by: Patroklos Papapetrou <ppapapetrou76@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -7,11 +7,10 @@ for item in *; do
     mv "$item" dir1/
   fi
 done
-git rm -r pkg specs agent internal go.* Dockerfile LICENSE OWNERS README.md docs hack codecov.yml Makefile sonar-project.properties .github/ .golangci.yaml .gitignore
+git rm -r pkg specs agent internal go.* Dockerfile LICENSE OWNERS README.md docs hack codecov.yml sonar-project.properties .github/ .golangci.yaml .gitignore
 rm dir1/sonar-project.properties dir1/codecov.yml
 mv dir1/ gitops-engine
 git checkout -b migrate
 git add gitops-engine
 git add prepare.sh
 git commit --signoff -m "prepare repo for migration to ArgoCD repo"
-git push --set-upstream origin migrate
