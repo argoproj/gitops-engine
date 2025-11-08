c5d383bc628b849acba49a0f38fc5b2c07949459
Peter Jiang <35584807+pjiang-dev@users.noreply.github.com>
2025-11-08 15:23:10 -0800

    fix(cherry-pick): regression on creationTimestamp with server-side diff
    Signed-off-by: Peter Jiang <peterjiang823@gmail.com>

--- pkg/diff/diff.go
+++ pkg/diff/diff.go
@@ -182,6 +182,9 @@ func serverSideDiff(config, live *unstructured.Unstructured, opts ...Option) (*D
 			return nil, fmt.Errorf("error removing non config mutations for resource %s/%s: %w", config.GetKind(), config.GetName(), err)
 		}
 	}
+	
+	// Remarshal predictedLive to ensure it receives the same normalization as live.
+	predictedLive = remarshal(predictedLive, o)
 
 	Normalize(predictedLive, opts...)
 	unstructured.RemoveNestedField(predictedLive.Object, "metadata", "managedFields")
